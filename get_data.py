70b7a7fa16a54036c0afcf4ebfecb729613e8a1e
John Stechschulte <john.l.stechschulte@gmail.com>
2015-01-29 07:45:54 -0800

    made user iterator

--- get_data.py
+++ get_data.py
@@ -2,21 +2,44 @@
 
 import bs4
 import urllib3
+import re
 from string import ascii_lowercase
 from random import randint
 
-USER_SEARCH = ('https://boardgamegeek.com/geeksearch.php?action=search&'+
-        'objecttype=user&q={}&B1=Go')
+def userGen():
+    USER_SEARCH = ('https://boardgamegeek.com/geeksearch.php?action=search&'+
+            'objecttype=user&q={}&B1=Go')
+    num_results_re = re.compile(r'Search Results \(([0-9]+) Matches?\)')
+    user_re = re.compile(r'href="/user/([^"]+)"')
 
-# first, build a list of 10000 users who have rated at least 50 games
-userList = []
-urllib3.disable_warnings()
-manager = urllib3.PoolManager()
+    urllib3.disable_warnings()
+    manager = urllib3.PoolManager()
 
-while len(userList) < 10000:
-    digraph = ''.join(ascii_lowercase[randint(0,25)] for i in range(2))
-    html = manager.request('GET', USER_SEARCH.format(digraph)).data
-    soup = bs4.BeautifulSoup(html, "lxml")
-    user = soup.find("div", "username")
-    print(user)
-    exit()
+    used_tris = set()
+
+    while True:
+        # search on a random trigraph
+        trigraph = ''.join(ascii_lowercase[randint(0,25)] for i in range(3))
+        while trigraph in used_tris:
+            trigraph = ''.join(ascii_lowercase[randint(0,25)] for i in range(3))
+        used_tris.add(trigraph)
+        html = manager.request('GET', USER_SEARCH.format(trigraph)).data
+
+        soup = bs4.BeautifulSoup(html, "lxml")
+        h2 = soup.find("h2")
+        m = num_results_re.search(h2.decode())
+        if m is None: continue
+        num_results = int(m.group(1))
+        for page in range(1, (num_results+99)//100 + 1):
+            if page>1:
+                html = manager.request('GET', USER_SEARCH.format(trigraph) +
+                        '&pageID={}'.format(page))
+                soup = bs4.BeautifulSoup(html, "lxml")
+            users = soup.find_all("div", "username")
+            for u in users:
+                m = user_re.search(u.decode())
+                if m is None: continue
+                yield m.group(1)
+
+for u in userGen():
+    print(u)
