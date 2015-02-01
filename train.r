3c0342099ae4eadafb3862dd75219d6ea3f76790
John Stechschulte <john.l.stechschulte@gmail.com>
2015-02-01 13:10:22 -0800

    bugfix, added hdf5, other stuff

--- train.r
+++ train.r
@@ -1,13 +1,15 @@
 library('Matrix')
 library('lsa')
+library('rhdf5')
 
 # cosine similarity function modified to ignore NA values
 # probably terribly inefficient
 na_cosine <- function(m) {
   c = matrix(0, nrow(m), nrow(m))
   for (i in 2:nrow(m)) {
-    for (j in 1:i-1) {
-      c[i,j] = cosine(na.omit(t(m[c(i, j),])))[1,2]
+    if (i%%100 == 0) {cat(i, '\n')}
+    for (j in 1:(i-1)) {
+      c[i,j] = cosine(na.omit(cbind(m[i,], m[j,])))[1,2]
     }
   }
   c = c + t(c)
@@ -28,7 +30,6 @@ topM = M[,1:NUM_GAMES]
 # calculate user similarity
 userSimilarity = na_cosine(topM)
 
-#predictRatings <- function(user) {
-#  weightedReviews = userSimilarity[,user] * topM[,-user]
-#
-#}
+h5createFile('data.h5')
+h5createGroup('data.h5', 'model')
+h5write(userSimilarity, 'data.h5', 'model/userSim')
