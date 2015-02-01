eb40c946b31fb8e2fe72925d01ab55b837dc8a50
John Stechschulte <john.l.stechschulte@gmail.com>
2015-02-01 15:18:31 -0800

    make_matrix.py updated to use hdf5

--- make_matrix.py
+++ make_matrix.py
@@ -2,6 +2,8 @@
 
 import bs4
 import glob
+import h5py
+import numpy as np
 import sys
 import os.path
 
@@ -10,6 +12,7 @@ games = dict()
 next_game = 0
 ratings = dict()
 
+print("reading data")
 for fn in glob.iglob('cached_users/*/*.xml'):
     with open(fn, "rb") as f:
         xml = f.read()
@@ -27,23 +30,44 @@ for fn in glob.iglob('cached_users/*/*.xml'):
             games[bggID] = 0
         games[bggID] += 1
         rating = gameSoup.findChild('rating').get('value')
-        ratings[userID, bggID] = rating
+        ratings[userID, bggID] = float(rating)
+
+print("done reading data")
+
+numUsers = len(users)
+maxUsernameLen = max(len(n) for n in users)
 
 # assign game IDs in decreasing order of number of reviews
 gameIDs = dict()
-next_gameID = 1
+next_gameID = 0
 for bggID, _ in sorted(games.items(), key=lambda x: x[1], reverse=True):
     gameIDs[bggID] = next_gameID
     next_gameID += 1
 
-with open(sys.argv[1], "w") as output_file:
+numGames = len(games)
+
+with h5py.File("data.h5") as f:
+    if 'training_data/ratings' in f:
+        del f['training_data/ratings']
+    print("writing data--ratings")
+    f.create_dataset('training_data/ratings', (numUsers, numGames), dtype='f')
+    # use NaN for missing data
+    f['training_data/ratings'][:] = float('NaN')
     for (userID, bggID), rating in ratings.items():
-        print(userID+1, gameIDs[bggID], rating, file=output_file)
+        f['training_data/ratings'][userID, gameIDs[bggID]] = rating
 
-with open(sys.argv[1]+'.users', "w") as output_file:
+    print("writing data--users")
+    if 'training_data/users' in f:
+        del f['training_data/users']
+    f.create_dataset('training_data/users', (numUsers,),
+            dtype='a'+str(maxUsernameLen))
     for i, u in enumerate(users):
-        print(i+1, u, file=output_file)
+        f['training_data/users'][i] = u.encode()
 
-with open(sys.argv[1]+'.games', "w") as output_file:
+    print("writing data--games")
+    if 'training_data/games' in f:
+        del f['training_data/games']
+    f.create_dataset('training_data/games', (numGames,),
+            dtype='i')
     for bggID, gameID in games.items():
-        print(gameID+1, bggID, file=output_file)
+        f['training_data/games'][gameID] = bggID
