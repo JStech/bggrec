bedd9df7f3e5fbcd75a3a58956bd1b72ca2059bd
John Stechschulte <john.l.stechschulte@gmail.com>
2015-02-01 18:04:11 -0800

    better test.r

--- test.r
+++ test.r
@@ -6,13 +6,22 @@ userSim <- h5read('data.h5', 'model/userSim')
 numUsers <- nrow(userSim)
 
 recommend <- function(user) {
+  # weight ratings by user similarity
   weightedRatings = userSim[,user] * ratings
-  gameSums = colSums(weightedRatings, na.rm = TRUE)
-  # arbitrary smoothing constant
-  numRatings = colSums(!is.na(weightedRatings)) + 5
-  predictedRating = gameSums / numRatings
+  # ignore user's own ratings (relevant to testing, irrelevant to actually
+  # making recommendations--we won't recommend something they've already rated)
+  weightedRatings = weightedRatings[-user,]
+
+  # find weighted averages
+  numerator = colSums(weightedRatings, na.rm = TRUE)
+  # add an arbitrary smoothing constant, so we don't just recommend games that
+  # one person rated 10/10
+  denominator = colSums(userSim[-user,user] * (!is.na(weightedRatings))) + 5
+  predictedRating = numerator / numRatings
+
+  # build output
   numGames = length(predictedRating)
-  game <- 1:numGames
-  rating <- ratings[user,]
-  return(data.frame(game, predictedRating, rating))
+  game <- (1:numGames)[order(-predictedRating)]
+  rating <- ratings[user,order(-predictedRating)]
+  return(data.frame(game, predictedRating[order(-predictedRating)], rating))
 }
